9a48dc3d00396b6f4bbac62b5bfa902a9eca08b7
Felix Breidenstein <felix@codemonauts.com>
2021-10-14 18:10:00 +0200

    Use global variables

--- proxy.go
+++ proxy.go
@@ -12,7 +12,12 @@ import (
 	"github.com/aws/aws-sdk-go/service/s3"
 )
 
-func handler(w http.ResponseWriter, r *http.Request, svc *s3.S3, bucket string) {
+var (
+	s3Service  *s3.S3
+	bucketName string
+)
+
+func handler(w http.ResponseWriter, r *http.Request) {
 	defer r.Body.Close()
 
 	key := r.URL.Path
@@ -23,10 +28,10 @@ func handler(w http.ResponseWriter, r *http.Request, svc *s3.S3, bucket string) 
 	}
 
 	input := &s3.GetObjectInput{
-		Bucket: aws.String(bucket),
+		Bucket: aws.String(bucketName),
 		Key:    aws.String(key),
 	}
-	obj, err := svc.GetObject(input)
+	obj, err := s3Service.GetObject(input)
 	if err != nil {
 		log.Printf("Error while getting %q: %s\n", key, err.Error())
 		w.WriteHeader(http.StatusForbidden)
@@ -53,21 +58,19 @@ func envOrDefault(name string, defaultValue string) string {
 func main() {
 	region := envOrDefault("S3PROXY_REGION", "eu-central-1")
 	port := envOrDefault("S3PROXY_PORT", "3000")
-	bucket := envOrDefault("S3PROXY_BUCKET", "")
+	bucketName = envOrDefault("S3PROXY_BUCKET", "")
 
-	if bucket == "" {
+	if bucketName == "" {
 		log.Fatal("You need to provide S3PROXY_BUCKET")
 	}
 
 	sess := session.Must(session.NewSession(&aws.Config{
 		Region: aws.String(region),
 	}))
-	svc := s3.New(sess)
+	s3Service = s3.New(sess)
 
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		handler(w, r, svc, bucket)
-	})
+	http.HandleFunc("/", handler)
 
-	fmt.Printf("Listening on :%s \n", port)
+	log.Printf("Listening on :%s \n", port)
 	log.Fatal(http.ListenAndServe(fmt.Sprintf(":%s", port), nil))
 }
